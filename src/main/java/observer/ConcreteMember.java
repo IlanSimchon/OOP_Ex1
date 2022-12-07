5762581b9bb2a45657c37c05da97ab2360e792db
alon <alonsuissa12@gmail.com>
2022-12-07 11:45:37 +0200

    basic project#2

--- src/main/java/observer/ConcreteMember.java
+++ src/main/java/observer/ConcreteMember.java
@@ -2,8 +2,11 @@ package observer;
 
 public class ConcreteMember implements Member{
     UndoableStringBuilder usb;
+    public ConcreteMember(){
+        usb = null;
+    }
     @Override
     public void update(UndoableStringBuilder usb) {
-    this.usb = usb;
+        this.usb = usb;
     }
 }
