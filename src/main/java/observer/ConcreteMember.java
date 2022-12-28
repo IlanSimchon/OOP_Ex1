f868b50cd86187bdc7a944c1a2ed64999bf5cbb8
alon <alonsuissa12@gmail.com>
2022-12-28 16:29:59 +0200

    added javadoc

--- src/main/java/observer/ConcreteMember.java
+++ src/main/java/observer/ConcreteMember.java
@@ -1,29 +1,48 @@
 package observer;
 
-
+/**
+ * an object that will register to group admin and will get updates about an undoableStringBuilder
+ */
 public class ConcreteMember implements Member{
+
     private String name;
     private UndoableStringBuilder usb;
    // private int num_of_changes;
-    private boolean is_conected;
+    private boolean is_connected;
     private int count_update; // for use in Tests
-
+    /**
+     * constructor
+     * @param name the name of the member
+     */
     public ConcreteMember(String name){
         this.name = name;
         usb = null;
-        is_conected = false;
+        is_connected = false;
     }
 
+    /**
+     * empty constructor
+     */
     public ConcreteMember(){
         name = "Plony";
         usb = null;
-        is_conected = false;
+        is_connected = false;
     }
+
+    /**
+     * updates the UndoableStringBuilder, and updates the number of updates.
+     * @param usb the UndoableStringBuilder that need to be updated
+     */
     @Override
     public void update(UndoableStringBuilder usb) {
         this.usb = usb;
         count_update++;
     }
+
+    /**
+     *
+     * @return the string of the undoableStringBuilder
+     */
     public String getString(){
         if(usb == null){
             return null;
@@ -31,27 +50,58 @@ public class ConcreteMember implements Member{
         return usb.toString();
     }
 
+    /**
+     *
+     * @return the undoableStringBuilder that this member holds.
+     */
     public UndoableStringBuilder getUsb(){
         return this.usb;
     }
 
+    /**
+     *
+     * @return returns the name of this member
+     */
     public String getName(){
         return name;
     }
 
-    public boolean getIs_conected() {
-        return is_conected;
+    /**
+     *
+     * @return true if this member is registered to an undoableStringBuilder
+     * and false if not.
+     */
+    public boolean getIs_connected() {
+        return is_connected;
     }
 
-    public void setIs_conected(boolean is_conected) {
-        this.is_conected = is_conected;
+    /**
+     *  set the parameter 'is_connected' to the input variable
+     * @param is_connected the new value
+     */
+    public void setIs_connected(boolean is_connected) {
+        this.is_connected = is_connected;
     }
+
+    /**
+     * make the updates counter zero.
+     */
     public void nullifyCount_update(){
         this.count_update = 0;
     }
+
+    /**
+     *
+     * @return the number of updates that maid in the undoableStringBuilder since the last registration
+     */
     public int getCount_update(){
         return  this.count_update;
     }
+
+    /**
+     *
+     * @return  the description of this ConcreteMember
+     */
     @Override
     public String toString() {
         return "ConcreteMember{" +
--- src/main/java/observer/GroupAdmin.java
+++ src/main/java/observer/GroupAdmin.java
@@ -4,6 +4,12 @@ import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
 
+/**
+ * an object that will manage a StringBuilder and group of members,
+ * and will update them every time that the undoableStringBuilder has changed.
+ * @author Ilan Simchon , Alon Suissa
+ * @version 1
+ */
 public class GroupAdmin implements Sender{
     private List<ConcreteMember> members;
     private UndoableStringBuilder usb;
@@ -12,17 +18,23 @@ public class GroupAdmin implements Sender{
         usb = new UndoableStringBuilder();
     }
 
+    /**
+     * registers the member obj to this GroupAdmin
+     * @param obj the member that will register to this GroupAdmin
+     * @throws UnsupportedOperationException if the member already register to another GroupAdmin
+     * @throws IllegalArgumentException if the input is null or not ConcreteMember
+     */
     @Override
     public void register(Member obj) throws UnsupportedOperationException {
         if (obj instanceof ConcreteMember) {
             ConcreteMember CMember = (ConcreteMember) obj;
             if (!this.members.contains(CMember)) {
-                if (CMember.getIs_conected()) {
+                if (CMember.getIs_connected()) {
                     throw new UnsupportedOperationException("you " +
                             "need to unregister your GroupAdmin before new registration");
                 } else {
                     members.add(CMember);
-                    CMember.setIs_conected(true);
+                    CMember.setIs_connected(true);
                     CMember.nullifyCount_update();
                 }
             }
@@ -30,17 +42,21 @@ public class GroupAdmin implements Sender{
              else {
                     throw new IllegalArgumentException("your input is null or other implementation of Member");
                 }
-
-
     }
 
+    /**
+     * unregisters the member obj from this GroupAdmin
+     * @param obj the member that will unregister
+     * @throws InputMismatchException if this GroupAdmin do not contain this ConcreteMember
+     * @throws IllegalArgumentException if the input is null or other implementation of Member
+     */
     @Override
     public void unregister(Member obj) {
         if(obj instanceof ConcreteMember) {
             ConcreteMember CMember = (ConcreteMember)obj;
             if(members.contains(CMember)) {
                 members.remove(CMember);
-                CMember.setIs_conected(false);
+                CMember.setIs_connected(false);
             }
             else {
                 throw new InputMismatchException("this GroupAdmin do not contain this ConcreteMember");
@@ -51,42 +67,83 @@ public class GroupAdmin implements Sender{
         }
     }
 
+    /**
+     * Inserts the string into this character sequence and updates all the members
+     * @param offset the index to make the insertion.
+     * @param obj a String.
+     *
+     */
     @Override
     public void insert(int offset, String obj) {
         usb.insert(offset,obj);
         updateAll();
     }
 
+    /**
+     * Appends the specified string to this character sequence and updates all the members.
+     * @param obj a String
+     * @return this UndoableStringBuilder with the changed String.
+     */
     @Override
     public void append(String obj) {
         usb.append(obj);
         updateAll();
     }
 
+    /**
+     * Removes the characters in a substring the undoableStringBuilder's String. The substring begins
+     * at the specified start and extends to the character at index
+     * end - 1 or to the end of the sequence if no such character exists.
+     * If start is equal to end, no changes are made.
+     * also the GroupAdmin updates all the members.
+     * @param start the index which the deletion starts from.
+     * @param end the index which the deletion ends on.
+     * @return this Object with the changed String.
+     */
     @Override
     public void delete(int start, int end) {
         usb.delete(start,end);
         updateAll();
     }
 
+    /**
+     * Undo the last change of the UndoableStringBuilder and updates all the members.
+     */
     @Override
     public void undo() {
         usb.undo();
         updateAll();
     }
 
+    /**
+     * updates all the members
+     */
     private void updateAll(){
         for (Member member: members) {
             member.update(usb);
         }
     }
+
+    /***
+     *
+     * @return the number of members this GroupAdmin have.
+     */
     public int getNumOfMembers(){
         return members.size();
     }
+
+    /**
+     *
+     * @return the string of the undoableStringBuilder
+     */
     public String getString(){
         return usb.toString();
     }
 
+    /**
+     *
+     * @return the description of this GroupAdmin
+     */
     @Override
     public String toString() {
         return "GroupAdmin{" +
